15b4ac304d5bc5386f6bde0590ea8d224c2483a7
Dream-Team-Pro <mz1320021@yahoo.com>
2020-11-22 00:47:04 +0300

    adding media query

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -7,5 +7,6 @@
     <link rel="stylesheet" href="resources/css/bootstrap.min.css">
     <link rel="stylesheet" href="resources/css/font-awesome.min.css">
     <link rel="stylesheet" href="resources/css/style.css">
+    <link rel="stylesheet" href="resources/css/media_style.css">
 </head>
 <body <?php if($page_title === "Admin Login | Badrblog") { echo "class='login-style'";} ?> >
