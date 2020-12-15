1b44bad408f8678af4d0574d56d463cdc0519a7a
Dream-Team-Pro <mz1320021@yahoo.com>
2020-12-16 00:57:33 +0300

    working at homepage

--- inc/navbar.php
+++ inc/navbar.php
@@ -2,6 +2,7 @@
 include "inc/init.php";
 foreach (get_settings() as $setting) {
     $name = $setting['name']; 
+    $logo = $setting['logo']; 
     $tagline = $setting['tagline'];
 }
 
@@ -19,7 +20,7 @@ foreach (get_settings() as $setting) {
 
 <div class="header">
     <nav class="navbar navbar-expand-lg navbar-dark ">
-    <a class="navbar-brand" href="#"><span>BADR</span><span>Blog</span></a>
+    <a class="navbar-brand" href="#"><img class="logo" src="admin/uploads/admins/<?php echo $logo; ?>"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -49,6 +50,9 @@ foreach (get_settings() as $setting) {
         <li class="nav-item">
             <a href="#" class="nav-link" tabindex="-1" aria-disabled="true"><i class="fa fa-search"></i></a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link sign-in" href="#" tabindex="-1" aria-disabled="true">Sign-in</a>
+        </li>        
         </ul>
         <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
